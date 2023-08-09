c62419ad487e7f3474eac59442ed27de1016e63f
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 <mohammadrezababaeimosleh@gmail.com>
2023-08-09 15:07:31 +0000

    edittng csv_edditor for rent transactions

--- src/csv_edit/main.py
+++ src/csv_edit/main.py
@@ -5,20 +5,32 @@ from src.Data import DATA_DIR
 
 logger.info("openning files...")
 
-old_transactions_df = pd.read_csv(DATA_DIR / "Transactions.csv")
+df_list = ["Transactions.csv", "Rent_Contracts.csv"]
+date_instance_list = ["instance_date", "contract_start_date"]
 
-logger.info("editting transactions file")
+# Adjust this value based on your system's memory capacity
+chunk_size = 1000000
 
-# Converting all data in column <instance_date> to date format for removing useless data
-old_transactions_df["instance_date"] = pd.to_datetime(
-    old_transactions_df["instance_date"]
-)
+for i in range(df_list):
+    old_transactions_df = pd.DataFrame()
 
-# Sorting by decending order
-old_transactions_df = old_transactions_df.sort_values("instance_date", ascending=False)
+    for chunk in pd.read_csv(DATA_DIR / df_list[i], chunksize=chunk_size):
+        old_transactions_df = pd.concat([old_transactions_df, chunk])
 
-logger.info("saving editted file")
+    logger.info("editting transactions file")
 
-old_transactions_df.to_csv(DATA_DIR / "Transactions.csv", mode="w", index=False)
+    # Converting all data in column <instance_date> to date format for removing useless data
+    old_transactions_df[date_instance_list[i]] = pd.to_datetime(
+        old_transactions_df[date_instance_list[i]]
+    )
 
-logger.info("editted file saved successfuly")
+    # Sorting by decending order
+    old_transactions_df = old_transactions_df.sort_values(
+        date_instance_list[i], ascending=False
+    )
+
+    logger.info("saving editted file")
+
+    old_transactions_df.to_csv(DATA_DIR / df_list[i], mode="w", index=False)
+
+    logger.info("editted file saved successfuly")
